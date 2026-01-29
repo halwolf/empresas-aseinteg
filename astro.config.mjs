ff96b067f58792b638cff5615781f17aabecd5a5
Jesus Martinez <jotamartinez1995@gmail.com>
2026-01-28 22:46:27 -0500

    Corrigiendo para github actions

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,9 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://halwolf.github.io',
+  base: '/empresas-aseinteg/',
   output: 'static',
+  trailingSlash: 'ignore',
   vite: {
     optimizeDeps:{
       include: ['astro-leaflet > leaflet'],
