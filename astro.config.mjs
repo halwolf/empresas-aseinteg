2dc240e16f096e5287c3a64beb977c0d27a4c3c7
Jesus Martinez <jotamartinez1995@gmail.com>
2026-02-01 21:29:50 -0500

    Mejora en copys, deploy para netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,11 @@
-// @ts-check
 import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
 export default defineConfig({
-  site: 'https://halwolf.github.io',
-  base: '/empresas-aseinteg/',
   output: 'static',
   trailingSlash: 'ignore',
   vite: {
-    optimizeDeps:{
+    optimizeDeps: {
       include: ['astro-leaflet > leaflet'],
     },
     plugins: [tailwindcss()],
