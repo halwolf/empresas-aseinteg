041f0c7f9234f770d442f1c27c771f550039acd9
Jesus Martinez <jotamartinez1995@gmail.com>
2026-02-03 22:16:10 -0500

    Anadido configuracion de SEO y posicionamiento en google search console

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap'
 
 export default defineConfig({
   output: 'static',
   trailingSlash: 'ignore',
+  site: 'https://aseinteg.com',
+  integrations: [sitemap()],
   vite: {
     optimizeDeps: {
       include: ['astro-leaflet > leaflet'],
